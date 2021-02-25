2b6071734f6071ad61a45c200ae1d886dc83179d
pdrolucasz <pedro.lucas4431@gmail.com>
2021-02-25 12:31:05 -0300

    adicionando informações do contexto e contexto no componente Countdown

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/pdrolucasz.png" alt="Pedro Lucas"/>
@@ -9,7 +13,7 @@ export function Profile() {
                 <strong>Pedro Lucas</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
