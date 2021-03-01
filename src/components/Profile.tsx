940c7f7e3f44843f8b588c01a62a5e9f0cb3aabc
pdrolucasz <pedro.lucas4431@gmail.com>
2021-03-01 13:34:35 -0300

    atualizando componente Profile para login

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,18 @@
 import { useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext'
+import { AuthContext } from '../contexts/AuthContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
     const { level } = useContext(ChallengesContext)
+    const { user } = useContext(AuthContext)
 
     return (
         <div className={styles.profileContainer}>
-            <img src="https://github.com/pdrolucasz.png" alt="Pedro Lucas"/>
+            <img src={`${user.avatarUrl}`} alt={user.name}/>
             
             <div>
-                <strong>Pedro Lucas</strong>
+                <strong>{user.name}</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
                     Level {level}
